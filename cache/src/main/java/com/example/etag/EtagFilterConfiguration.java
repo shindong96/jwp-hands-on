5f3585402a81c5a535ae9286a120af96aa0d0fe3
shindong96 <2006peter@naver.com>
2022-09-02 20:56:43 +0900

    feat: 캐시 무효화

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,18 +1,24 @@
 package com.example.etag;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+import com.example.version.ResourceVersion;
+
 @Configuration
 public class EtagFilterConfiguration {
+
+    @Autowired
+    private ResourceVersion resourceVersion;
 
     @Bean
     public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
         FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
             = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
-        filterRegistrationBean.addUrlPatterns("/etag");
+        filterRegistrationBean.addUrlPatterns("/etag", "/resources/" + resourceVersion.getVersion() + "/js/index.js");
         return filterRegistrationBean;
     }
 }
