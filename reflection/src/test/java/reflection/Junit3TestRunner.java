bc0121790a60077225f985564d9dec0addf86ad1
shindong96 <2006peter@naver.com>
2022-09-16 14:47:15 +0900

    feat: reflection 실습테스트 통과

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -8,6 +9,12 @@ class Junit3TestRunner {
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.getName().contains("test")) {
+                declaredMethod.invoke(new Junit3Test());
+            }
+        }
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
     }
 }
--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,6 +9,12 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.isAnnotationPresent(MyTest.class)) {
+                declaredMethod.invoke(new Junit4Test());
+            }
+        }
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
     }
 }
