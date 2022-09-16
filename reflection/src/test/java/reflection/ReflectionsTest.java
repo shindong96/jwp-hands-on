f7502c0344256e0e612ec79247e65f467bd91fa5
Dongseok Shin <70707629+shindong96@users.noreply.github.com>
2022-09-16 20:05:24 +0900

    feat: reflection 실습테스트 통과 (#3)

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -12,7 +16,20 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
+        final Set<Class<?>> clazzWithControllerAnnotation = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : clazzWithControllerAnnotation) {
+            log.info(clazz.getName());
+        }
 
+        final Set<Class<?>> clazzWithServiceAnnotation = reflections.getTypesAnnotatedWith(Service.class);
+        for (Class<?> clazz : clazzWithServiceAnnotation) {
+            log.info(clazz.getName());
+        }
+
+        final Set<Class<?>> clazzWithRepositoryAnnotation = reflections.getTypesAnnotatedWith(Repository.class);
+        for (Class<?> clazz : clazzWithRepositoryAnnotation) {
+            log.info(clazz.getName());
+        }
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
     }
 }
